20fd86ecee709f822d2ec3df2e76d6a576fb5a9b
brendonlucas <brendonplay007@gmail.com>
2020-05-13 18:50:26 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,41 +10,28 @@ void main() {
           backgroundColor: Colors.lightBlue[500],
         ),
         body: Builder(
-          builder: (context) =>
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    final snackBar = SnackBar(content: Text('Diamante'),
-                    action: SnackBarAction(
+          builder: (context) => Center(
+            child: GestureDetector(
+              onTap: () {
+                final snackBar = SnackBar(
+                  content: Text('Diamante'),
+                  action: SnackBarAction(
                     label: 'Fechar',
                     onPressed: () {},
-                    ),
-                    );
-                    Scaffold.of(context).showSnackBar(snackBar);
-                  },
-
-                  child: Image(
-                    image: AssetImage('images/diamond.png'),
-                    width: 300,
-                    height: 300,
-                    fit: BoxFit.fill,
                   ),
-                ),
+                );
+                Scaffold.of(context).showSnackBar(snackBar);
+              },
+              child: Image(
+                image: AssetImage('images/diamond.png'),
+                width: 300,
+                height: 300,
+                fit: BoxFit.fill,
               ),
-        ),)
-      ,
-    )
-    ,
-  );
-}
-
-void _showToast(BuildContext context) {
-  final scaffold = Scaffold.of(context);
-  scaffold.showSnackBar(
-    SnackBar(
-      content: const Text('Added to favorite'),
-      action: SnackBarAction(
-          label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
